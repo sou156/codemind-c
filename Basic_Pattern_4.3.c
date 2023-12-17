f032d4817ebb3bf72fc8a705a18f1dfeb97f42f0
sou156 <149807337+sou156@users.noreply.github.com>
2023-12-18 00:54:38 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     for(int i=1;i<=a;i++){         for(int j=1;j<=a;j++){             if(j%2==0){                 printf("0 ");             }             else {                 printf("1 ");             }         }         printf(" ");     } }

---
+++ Basic_Pattern_4.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=a;j++){
+            if(j%2==0){
+                printf("0 ");
+            }
+            else {
+                printf("1 ");
+            }
+        }
+        printf("
+");
+    }
+}
