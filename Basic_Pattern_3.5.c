25cc2eefcb6c0f7fc63a832082e0932570c1705f
sou156 <149807337+sou156@users.noreply.github.com>
2023-12-18 01:06:31 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     for(int i=1;i<=a;i++){         for(int k=1;k<=a-i+1;k++){             printf("%c ",64+k);         }          for(int j=1;j<=i-1;j++){             printf("");         }         printf(" ");     } }

---
+++ Basic_Pattern_3.5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        for(int k=1;k<=a-i+1;k++){
+            printf("%c ",64+k);
+        }
+         for(int j=1;j<=i-1;j++){
+            printf("");
+        }
+        printf("
+");
+    }
+}
