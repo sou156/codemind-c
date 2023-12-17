22316c305f175c3c79e325430f70f7a00338ccde
sou156 <149807337+sou156@users.noreply.github.com>
2023-12-17 21:00:56 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     for(int i=a;i>=1;i--){         for(int j=1;j<=a;j++){             if(i==j || i+j==a+1){                 printf("%d",i);             }             else{                 printf(" ");             }
            }
            printf("
    ");
        }
    }

---
+++ Pattern_8.3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    for(int i=a;i>=1;i--){
+        for(int j=1;j<=a;j++){
+            if(i==j || i+j==a+1){
+                printf("%d",i);
+            }
+            else{
+                printf(" ");
+            }
+            
+        }
+        printf("
+");
+    }
+}
