081ed97dd83a4c956540cd8bbcd20e605254225b
sou156 <149807337+sou156@users.noreply.github.com>
2023-12-24 23:58:02 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     int arr[a];     for(int i=0;i<a;i++){         scanf("%d",&arr[i]);     }     int sum=0;     for(int i=0;i<a;i++){         if(i%2!=0){             sum=sum+arr[i];         }
        }
        printf("%d",sum);
    }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(int i=0;i<a;i++){
+        if(i%2!=0){
+            sum=sum+arr[i];
+        }
+       
+    }
+    printf("%d",sum);
+}
