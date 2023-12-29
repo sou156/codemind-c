7f44960d7ad7221b587d4b29019e13338564d016
sou156 <149807337+sou156@users.noreply.github.com>
2023-12-29 22:09:03 +0530

    #include<stdio.h> #include<stdlib.h> int main(){     int a;     scanf("%d",&a);     int arr[a];     for(int i=0;i<a;i++){         scanf("%d",&arr[i]);     } int flag=0;     for(int i=0;i<a;i++){         if(arr[i]%2!=0){             if(i%2!=0){                 flag++;             }             else{                 printf("False");                 exit(0);             }         }     }     if(flag>=1){         printf("True");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+#include<stdlib.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for(int i=0;i<a;i++){
+        scanf("%d",&arr[i]);
+    } int flag=0;
+    for(int i=0;i<a;i++){
+        if(arr[i]%2!=0){
+            if(i%2!=0){
+                flag++;
+            }
+            else{
+                printf("False");
+                exit(0);
+            }
+        }
+    }
+    if(flag>=1){
+        printf("True");
+    }
+}
